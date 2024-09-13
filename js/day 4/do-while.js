421cca781203cb7c3396df52b0e65c431a68349e
Vinay Kumar <vinaysikhera2203@gmail.com>
2024-09-13 10:49:28 +0530

    js fundamantal 2 assignment

--- js/day 4/do-while.js
+++ js/day 4/do-while.js
@@ -1,9 +1,9 @@
 // Basic Do-while loop==================
-let i=0;
+let i = 0;
 do {
-//   console.log(i);
+    //   console.log(i);
     i++;
-} while (i<5);
+} while (i < 5);
 // input Validation Using Do-While
 
 function validatePassword() {
@@ -12,23 +12,33 @@ function validatePassword() {
         // password=prompt("please enter your password will be in 6 digits");
 
         if (password.length >= 6) {
-            alert("password is valid ")   
+            alert("password is valid ")
             break;
         } else {
             alert("password is too short ")
-         
+
         }
-        
-    } while (password.length<=6);
+
+    } while (password.length <= 6);
 }
 // validatePassword()
 
 // random number guessing game;--------------------------------------------------
 
-function guessNumber(num) {
-       do {
-        let randomNumber= Math.floor(Math.random()*10);
-        console.log(randomNumber);
-    } while (num >=10);
+function guessNumber() {
+
+    let clintGess ; 
+    let generateRandomNumber;
+    do {
+       clintGess= prompt('enter your gess number 1 to 10')
+        generateRandomNumber = Math.floor(Math.random() * 10)+1
+        if (Number(clintGess) == generateRandomNumber) {
+            alert("Conguralation you won the match");
+            break;
+        } else {
+            alert("best but try again");
+            // clintGess= prompt("gess with a new value");
+        }
+    } while (Number(clintGess) != generateRandomNumber);
 }
 guessNumber()
