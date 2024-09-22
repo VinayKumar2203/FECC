66aae1134a85900ba7aa10078ede0abbedc98f70
Vinay Kumar <vinaysikhera2203@gmail.com>
2024-09-22 10:19:35 +0530

    day 7 assignment of 5

--- js/day 7/Constuctor.js/prob5.js
+++ js/day 7/Constuctor.js/prob5.js
@@ -16,17 +16,22 @@ class Library {
     constructor() {
         this.books = [];
     }
-    addBooks(books) {
-        return this.books.push(this.books);
+    addBooks(book) {
+        return this.books.push(book);
     }
     filter(){
-         if(this.item <=2012){
-           return this.title;
-         }
+       return this.books.filter((item)=>item.year<=2012)
+        .map((arr)=>arr.title)
+        .reduce((acc,curr)=>acc +' '+ curr,'')
     }
 }
 
 let b1 = new Book("rich and poor", "vinay", 2022, "how to rich");
 console.log(b1);
-let a1 = new Library()
-console.log(a1.addBooks())
+let a1 = new Library();
+// console.log(a1.filter(b1))
+a1.addBooks(b1);
+console.log(a1.filter());
+let b2 = new Book("jay ", "Tulsidas", 2010, "history");
+a1.addBooks(b2); 
+console.log(a1.filter());
