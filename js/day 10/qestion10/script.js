0bb58d5fa123697eab2da7dae2fc467d89d2e11e
Vinay Kumar <vinaysikhera2203@gmail.com>
2024-09-28 14:03:41 +0530

    day 10 and question10

---
+++ js/day 10/qestion10/script.js
@@ -0,0 +1,28 @@
+let start = document.querySelector('#start');
+let pause = document.getElementById('pause');
+let reset = document.getElementById('reset');
+let mint = document.getElementById('mint');
+let sec = document.getElementById('sec');
+
+let timer;
+let second=0;
+let minutes=0;
+start.addEventListener('click', () => {
+    timer = setInterval(() => {
+        second++
+        if (second > 60) {
+            second=0;
+            minutes++;
+            mint.innerText=minutes;
+        }
+        sec.innerText=second;
+    }, 1000);
+})
+pause.addEventListener('click', () => {
+    clearInterval(timer);
+    console.log(timer)
+})
+reset.addEventListener('click', () => {
+    clearInterval(timer);
+    window.location.reload();
+})
