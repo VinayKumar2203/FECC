c7ef8703b349904b421e053ad400818de5dee67e
Vinay Kumar <vinaysikhera2203@gmail.com>
2024-11-21 00:32:59 +0530

    ref question 2

--- React/day8/useRefQuestion2/src/AdvanceUseRef.jsx
+++ React/day8/useRefQuestion2/src/AdvanceUseRef.jsx
@@ -1,66 +1,50 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from 'react'
 
 const AdvanceUseRef = () => {
+  let getData = {};
 
-  let [inputDet, setInputDet] = useState({ name: "", email: "" });
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log("form submitted", inputDet);
-  }
-
-  function handleChangeName(e) {
-        setInputDet(e.target.value);
-
-    // setInputDet({ ...inputDet, [e.target.name]: e.target.value });
-    console.log(e.target.value);
-  }
-  function handleChangeEmail(e) {
-    setInputDet(e.target.value);
-    // setInputDet({ ...inputDet, [e.target.name]: e.target.value });
-    console.log(e.target.value);
-
-  }
+  let emailRef = useRef();
+  let passwordRef = useRef();
+  let spanEmailRef = useRef(null)
+  let spanPassWordRef = useRef(null)
 
 
-  const nameRef = useRef(null);
-  const emailRef = useRef(null);
+  // console.log(emailRef)
+  // console.log(getData)
 
 
   useEffect(() => {
-    nameRef.current.focus();
+    emailRef.current.focus();
+
   }, [])
 
-  function handleFocus(emailRef) {
-    if (emailRef.current) {
-      emailRef.current.focus();
-    }
-  }
 
+  function handleSubmitForm(e) {
+    e.preventDefault();
+    getData["email"] = emailRef.current.value;
+    getData["Password"] = passwordRef.current.value;
+    console.log(getData)
+    console.log(emailRef, "email")
+    console.log(passwordRef, "PasswordRef")
+    if (emailRef.current.value == "" && passwordRef.current.value == "") {
+      spanEmailRef.current.innerHTML = "Please enter a valid email"
+      spanEmailRef.current.style.color = "red";
+      spanPassWordRef.current.style.color = "red";
+      spanPassWordRef.current.innerHTML = "Please enter a valid Password"
+    }
+
+  }
 
   return (
     <div>
-      <form onClick={handleSubmit}>
-        <label>
-          name:-
-          <input
-            ref={nameRef}
-            type="text"
-            placeholder="name.."
-            value={inputDet.name}
-            onChange={handleChangeName}
-          />
+      <form onSubmit={handleSubmitForm}>
+        <label> email:-
+          <input ref={emailRef} type="text" placeholder='enter you email' />
+          <span ref={spanEmailRef} id='email'></span>
         </label>
-        <label>
-          email:-
-          <input
-            ref={emailRef}
-            type="email"
-            placeholder="email..."
-            value={inputDet.email}
-            onChange={handleChangeEmail}
-            onFocus={()=>handleFocus(emailRef)}
-          />
+        <label> Password:-
+          <input ref={passwordRef} type="text" placeholder='enter you password' />
+          <span ref={spanPassWordRef} id='password'></span>
         </label>
         <input type="submit" value="submit" />
       </form>
